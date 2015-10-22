b930311505cd66910cc6418fc0591dcf0a00556e
ishuen <sandy051122@gmail.com>
2015-10-22 22:54:24 +0800

    add public file with js & css, postgresql connection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express'),
   http = require('http'),
   path = require('path'),
   passport = require("passport");
+var pg = require('pg');
+var conString = "postgres://wu:@localhost/wu";
 
 var env = process.env.NODE_ENV || 'development',
   config = require('./config/config')[env];
@@ -58,6 +60,20 @@ app.use(function(req, res, next){
   res.type('txt').send('Not found');
 });
 
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
 
 require('./config/routes')(app, config, passport);
 
